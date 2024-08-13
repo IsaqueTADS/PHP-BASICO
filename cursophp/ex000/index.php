a90f22e1ec756bd688693d0c674639ab0abfff5a
IsaqueTADS <isaqueofc18@gmail.com>
2024-08-12 21:41:32 -0300

    Primeiro exercicios de php moderno do guanabara

---
+++ cursophp/ex000/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primiero Código</title>
+</head>
+
+<body>
+    <h1>
+
+        <?php
+
+        echo "Hello World \u{1F30E}"; 
+        print "Olá Mundo"
+        
+        
+        ?>
+
+
+    </h1>
+    <p>Jesus levou toda maldição ná cruz!!</p>
+
+</body>
+
+</html>
---
+++ cursophp/ex003/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt_br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos em PHP</title>
+</head>
+
+<body>
+
+    <h1>Teste de tipos primitivos</h1>
+
+    <?php
+    // ox = heaxadacimal ob = binário 0 = Octal
+    // $num = 0x1ABCEF2345;
+    //echo "O numero tem valor $num ";
+
+    //$num = 3010;
+    //var_dump($num);
+
+        
+    //$num = 3e2; // 3 x 10(2)
+    //echo"O valor é $num";
+
+    //$num = (integer) 5e10; //coerção
+    //var_dump($num);
+
+    //$casado = true;
+
+    //var_dump($casado);
+
+    //echo"o valor de casadp é $casado";
+
+    
+        
+
+    ?>
+
+</body>
+
+</html>
